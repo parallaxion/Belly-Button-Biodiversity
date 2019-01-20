1613f6cc2321f24233ccde6fc0cfa4ceeefa6f43
Jacob Lingwall <lingwaja@nga.mil>
2019-01-19 22:01:45 -0600

    git is lame

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// from data.js
+var tableData = data;
+//http://bl.ocks.org/jfreels/6734025
+//https://wustl.bootcampcontent.com/wustl-bootcamp/WASHSTL201809DATA3/tree/master/14-Intro-To-JavaScript/Homework/Instructions
+// YOUR CODE HERE!
